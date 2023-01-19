96272ceb854324ffc27d609fd3f2983c2db63781
Vibhor Sheshrao Patil <121420092+iamvibh0r@users.noreply.github.com>
2023-01-19 15:20:52 +0530

    Update index.js
    Added Some js

--- index.js
+++ index.js
@@ -23,3 +23,34 @@ function age(){
     document.getElementById("age").innerHTML = "Your Age is " + y + " Years " + m + " Months " + d + " Days ";
 }
 
+
+
+document.addEventListener("contextmenu",function(e){
+  alert("Code Available on github");
+  e.preventDefault();
+});
+
+document.onkeydown = function(e){
+  if(event.keyCode == 123){
+    alert("Code Available on github");
+    return false;
+  }
+
+  if(e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)){
+    alert("Code Available on github");
+    return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)){
+    alert("Code Available on github");
+    return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)){
+    alert("Code Available on github");
+    return false;
+  }
+  if(e.ctrlKey && e.keyCode == "U".charCodeAt(0)){
+    alert("Code Available on github");
+    return false;
+    
+  }
+};
